1a21ea537ad4548cff3ac9ece37c7765ab501884
Samuel Nitsch <stoikonitsch@gmail.com>
2019-01-02 17:19:42 +0100

    improved animation (draw every 9ms, change animation every 10th frame)

---
+++ src/javascript/animation.js
@@ -0,0 +1,76 @@
+var ticks = {
+    player1_tick : 0,
+    player2_tick : 0,
+    monster_tick : 0
+}
+var frame_cnt = 0;
+
+class AnimationFrame {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Character {
+    constructor(row, tick) { //which row on asset png is player/monster
+        this.direction = new Array(3); //up, down, right
+        let y = 16 * row;
+
+        //up
+        this.direction[0] = new Array(3);
+        this.direction[0][0] = new AnimationFrame(0, y); //idle
+        this.direction[0][1] = new AnimationFrame(8 * 16, y); //animation up #1
+        this.direction[0][2] = new AnimationFrame(9 * 16, y); //animation up #
+
+        //down
+        this.direction[1] = new Array(3);
+        this.direction[1][0] = new AnimationFrame(1 * 16, y); //idle
+        this.direction[1][1] = new AnimationFrame(2 * 16, y); //animation down #1
+        this.direction[1][2] = new AnimationFrame(3 * 16, y); //animation down #2
+
+        //right
+        this.direction[2] = new Array(4);
+        this.direction[2][0] = new AnimationFrame(4 * 16, y); //idle
+        this.direction[2][1] = new AnimationFrame(5 * 16, y); //animation right #1
+        this.direction[2][2] = new AnimationFrame(6 * 16, y); //animation right #2
+        this.direction[2][3] = new AnimationFrame(7 * 16, y); //animation right #3
+
+        this.last_direction = "up"; //up per default
+        this.tick = tick;
+        ticks[this.tick] = 0;
+    }
+
+
+    getAnimation(movement) {
+        if(frame_cnt % 15 === 0){ //every 10th frame, a new animation image is shown
+            ticks[this.tick] += 1; //counts next animation
+        }
+        if (movement !== this.last_direction) { //direction changed
+            ticks[this.tick] = 1; // 0 would be idle, 1 is first moving motion
+        }else if(ticks[this.tick] === 12){
+            ticks[this.tick] = 0;
+        }
+        this.last_direction = movement;
+
+        switch (movement) {
+            case "up":
+                return this.direction[0][ticks[this.tick] % 3];
+            case "down":
+                return this.direction[1][ticks[this.tick] % 3];
+            case "right":
+            case "left":
+                return this.direction[2][ticks[this.tick] % 4];
+            default:
+                return this.direction[0][0];
+        }
+    }
+
+}
+
+class Bomb{
+    constructor(){
+        
+    }
+
+}
--- src/javascript/boardLogic.js
+++ src/javascript/boardLogic.js
@@ -51,8 +51,19 @@ function initGameboard(board, width, height) {
 }
 
 
+
+
+let bomberman = new Character(4, 0);
+let bomberman2 = new Character(3, 1);
+let bomberman3 = new Character(2, 2);
+var cnt = 60;
 //draws the gameboard part within the canvas
 function drawGameboard(data, ctx, width, height) {
+
+    cnt = (cnt % (height * 60)) + 1;
+    if (cnt > (height - 2) * 60) cnt = 60;
+
+
     //col = x coordinates, row = y
     for (var row = 0; row < height; row++) {
         for (var col = 0; col < width; col++) {
@@ -63,4 +74,16 @@ function drawGameboard(data, ctx, width, height) {
             }
         }
     }
+    let animation2 = bomberman2.getAnimation("down");
+    ctx.drawImage(document.getElementById('art_assets'),
+        animation2.x, animation2.y, 16, 16, 1 * tileSize, (cnt / 60) * tileSize, tileSize, tileSize);
+
+    let animation3 = bomberman3.getAnimation("right");
+    ctx.drawImage(document.getElementById('art_assets'),
+        animation3.x, animation3.y, 16, 16, 3 * tileSize, (cnt / 60) * tileSize, tileSize, tileSize);
+
+    let animation = bomberman.getAnimation("up");
+    ctx.drawImage(document.getElementById('art_assets'),
+        animation.x, animation.y, 16, 16, 5 * tileSize, (cnt / 60) * tileSize, tileSize, tileSize);
+
 }
